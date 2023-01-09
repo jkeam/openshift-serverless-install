2a0c7d7eecfd41b24ba350c181a485aabcd74f9e
Jon Keam <jpkeam@gmail.com>
2023-01-09 11:27:42 -0500

    remove unneeded log info

--- install.sh
+++ install.sh
@@ -46,12 +46,7 @@ poll_for_operator_creation
 echo 'installing serving...'
 oc create -f ./serving.yaml
 poll_for_serving_creation
-oc get knativeserving.operator.knative.dev/knative-serving -n knative-serving --template='{{range .status.conditions}}{{printf "%s=%s\n" .type .status}}{{end}}'
-oc get pods -n knative-serving
-oc get pods -n knative-serving-ingress
 
 echo 'installing eventing...'
 oc apply -f ./eventing.yaml
 poll_for_eventing_creation
-oc get knativeeventing.operator.knative.dev/knative-eventing -n knative-eventing --template='{{range .status.conditions}}{{printf "%s=%s\n" .type .status}}{{end}}'
-oc get pods -n knative-eventing
