d708383da1c062e84cb93772f46073aa85d963f5
Jon Keam <jpkeam@gmail.com>
2023-01-09 11:26:53 -0500

    fixed bad copy and paste and also added intelligent polling instead of sleep

--- install.sh
+++ install.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+poll_for_serving_creation() {
+  while true; do
+    OUTPUT=$(oc get knativeserving.operator.knative.dev/knative-serving -n knative-serving --template='{{range .status.conditions}}{{printf "%s=%s\n" .type .status}}{{end}}' | grep 'Ready')
+    if [[ "$OUTPUT" == "Ready=True" ]];
+    then
+      break
+    else
+      sleep 10
+      OUTPUT=$(oc get knativeserving.operator.knative.dev/knative-serving -n knative-serving --template='{{range .status.conditions}}{{printf "%s=%s\n" .type .status}}{{end}}' | grep 'Ready')
+    fi
+  done
+}
+
+poll_for_eventing_creation() {
+  while true; do
+    OUTPUT=$(oc get knativeeventing.operator.knative.dev/knative-eventing -n knative-eventing --template='{{range .status.conditions}}{{printf "%s=%s\n" .type .status}}{{end}}' | grep 'Ready')
+    if [[ "$OUTPUT" == "Ready=True" ]];
+    then
+      break
+    else
+      sleep 10
+      OUTPUT=$(oc get knativeeventing.operator.knative.dev/knative-eventing -n knative-eventing --template='{{range .status.conditions}}{{printf "%s=%s\n" .type .status}}{{end}}' | grep 'Ready')
+    fi
+  done
+}
+
 poll_for_operator_creation() {
   while true; do
     OUTPUT=$(oc get csv -n openshift-serverless -o json | jq '[.items[0].status.phase][0]')
@@ -19,19 +45,13 @@ poll_for_operator_creation
 
 echo 'installing serving...'
 oc create -f ./serving.yaml
-sleep 60
+poll_for_serving_creation
 oc get knativeserving.operator.knative.dev/knative-serving -n knative-serving --template='{{range .status.conditions}}{{printf "%s=%s\n" .type .status}}{{end}}'
-# DependenciesInstalled=True
-# DeploymentsAvailable=True
-# InstallSucceeded=True
-# Ready=True
 oc get pods -n knative-serving
 oc get pods -n knative-serving-ingress
 
 echo 'installing eventing...'
 oc apply -f ./eventing.yaml
-sleep 60
+poll_for_eventing_creation
 oc get knativeeventing.operator.knative.dev/knative-eventing -n knative-eventing --template='{{range .status.conditions}}{{printf "%s=%s\n" .type .status}}{{end}}'
-# InstallSucceeded=True
-# Ready=True
 oc get pods -n knative-eventing
